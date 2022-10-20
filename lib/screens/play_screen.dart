d4a6e682d59a5d7a4786b36401d2c4e3c3c2101d
Samson BADAYODI <sbadayodi@gmail.com>
2022-10-20 01:50:52 +0000

    update play-screen ui

--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -15,13 +15,13 @@ class PlayMusique extends StatefulWidget {
 class _PlayMusiqueState extends State<PlayMusique> {
   List<Musique> musiques = [
     Musique(
-        title: "musique1",
-        artiste: "artiste1",
+        title: "Chants du Nouveau Monde",
+        artiste: "Hcham Chahidi",
         imagepath: "assets/disque.png",
         urlSong: "Carte_aux_Adieux.mp3"),
     Musique(
-        title: "musique2",
-        artiste: "artiste2",
+        title: "Vertu",
+        artiste: "Hcham Chahidi",
         imagepath: "assets/man.jpg",
         urlSong: "Vertu.mp3")
   ];
@@ -72,8 +72,8 @@ class _PlayMusiqueState extends State<PlayMusique> {
     await player.play(source);
     Duration? dure = await player.getDuration();
     setState(() {
-      status = PlayerState.playing;
       duree = dure;
+      status = PlayerState.playing;
     });
   }
 
@@ -132,57 +132,175 @@ class _PlayMusiqueState extends State<PlayMusique> {
         title: const Text("Musique Player"),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Card(
-                elevation: 9.0,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.height / 2.5,
-                  child: Image.asset(musiqueActuelle.imagepath),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.blue, Colors.indigo],
+        )),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 48.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Text(
+                  musiqueActuelle.title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              textStyle(musiqueActuelle.title, 1.5),
-              textStyle(musiqueActuelle.artiste, 1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  playButton(Icons.fast_rewind, 30.0, ActionMusic.rewind),
-                  playButton(
-                      (status == PlayerState.playing)
-                          ? Icons.pause
-                          : Icons.play_arrow,
-                      35.0,
-                      (status == PlayerState.playing)
-                          ? ActionMusic.pause
-                          : ActionMusic.play),
-                  playButton(Icons.fast_forward, 30.0, ActionMusic.forward)
-                ],
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Text(
+                  musiqueActuelle.artiste,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  textStyle(fromDuration(position), 0.8),
-                  textStyle(fromDuration(duree!), 0.8),
-                ],
+              Center(
+                child: Container(
+                  width: 300.0,
+                  height: 300.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      image: DecorationImage(
+                          image: AssetImage(musiqueActuelle.imagepath))),
+                ),
               ),
-              Slider(
-                  value: position.inSeconds.toDouble(),
-                  min: 0,
-                  max: duree!.inSeconds.toDouble(),
-                  onChanged: (double d) {
-                    setState(() {
-                      // position = Duration(seconds: d.toInt());
-                      player.seek(Duration(seconds: d.toInt()));
-                    });
-                  })
-            ]),
+              const SizedBox(
+                height: 18.0,
+              ),
+              const Center(
+                child: Text(
+                  ".",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Expanded(
+                  child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 500,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          textStyle(fromDuration(position), 1.0),
+                          Slider(
+                              value: position.inSeconds.toDouble(),
+                              min: 0,
+                              max: duree!.inSeconds.toDouble(),
+                              onChanged: (double d) {
+                                setState(() {
+                                  // position = Duration(seconds: d.toInt());
+                                  player.seek(Duration(seconds: d.toInt()));
+                                });
+                              }),
+                          textStyle(fromDuration(duree!), 1.0),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        playButton(Icons.skip_previous, 45, Colors.blue,
+                            ActionMusic.rewind),
+                        playButton(
+                            (status == PlayerState.playing)
+                                ? Icons.pause
+                                : Icons.play_arrow,
+                            62,
+                            Colors.blueAccent,
+                            (status == PlayerState.playing)
+                                ? ActionMusic.pause
+                                : ActionMusic.play),
+                        playButton(Icons.skip_next, 45, Colors.blue,
+                            ActionMusic.forward),
+                      ],
+                    )
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
       ),
+      // Center(
+      //   child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //       children: <Widget>[
+      //         Card(
+      //           elevation: 9.0,
+      //           child: SizedBox(
+      //             width: MediaQuery.of(context).size.height / 2.5,
+      //             child: Image.asset(musiqueActuelle.imagepath),
+      //           ),
+      //         ),
+      //         textStyle(musiqueActuelle.title, 1.5),
+      //         textStyle(musiqueActuelle.artiste, 1),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: <Widget>[
+      //             playButton(Icons.fast_rewind, 30.0, ActionMusic.rewind),
+      //             playButton(
+      //                 (status == PlayerState.playing)
+      //                     ? Icons.pause
+      //                     : Icons.play_arrow,
+      //                 35.0,
+      //                 (status == PlayerState.playing)
+      //                     ? ActionMusic.pause
+      //                     : ActionMusic.play),
+      //             playButton(Icons.fast_forward, 30.0, ActionMusic.forward)
+      //           ],
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: <Widget>[
+      //             textStyle(fromDuration(position), 0.8),
+      //             textStyle(fromDuration(duree!), 0.8),
+      //           ],
+      //         ),
+      //         Slider(
+      //             value: position.inSeconds.toDouble(),
+      //             min: 0,
+      //             max: duree!.inSeconds.toDouble(),
+      //             onChanged: (double d) {
+      //               setState(() {
+      //                 // position = Duration(seconds: d.toInt());
+      //                 player.seek(Duration(seconds: d.toInt()));
+      //               });
+      //             })
+      //       ]),
+      // ),
     );
   }
 
-  Widget playButton(IconData icon, double taille, ActionMusic action) {
+  Widget playButton(
+      IconData icon, double taille, Color color, ActionMusic action) {
     return IconButton(
       onPressed: () {
         switch (action) {
@@ -202,6 +320,7 @@ class _PlayMusiqueState extends State<PlayMusique> {
       },
       icon: Icon(icon),
       iconSize: taille,
+      color: color,
     );
   }
 }
